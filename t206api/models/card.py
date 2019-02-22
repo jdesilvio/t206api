593220e75ca1a8e1a836a369adc09eaf3db68001
John DeSilvio <john.desilvio@yahoo.com>
2019-02-21 19:31:22 -0500

    Python 2/3 compatibility

--- t206api/models/card.py
+++ t206api/models/card.py
@@ -2,6 +2,11 @@
 Card model
 """
 
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
 from ..db import db
 
 
